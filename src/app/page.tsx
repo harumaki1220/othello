b7280e86eca1c105e70f043fe1eb998838d72383
harumaki1220 <haruma181220@gmail.com>
2025-04-29 14:15:59 +0900

    if文からfor文へ移行作戦

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -20,6 +20,7 @@ export default function Home() {
     const newBoard = structuredClone(board);
     let h: number = 0;
     if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
+      console.log(h);
       newBoard[y][x] = turnColor;
       setTurnColor(2 / turnColor);
       setboard(newBoard);
@@ -47,7 +48,7 @@ export default function Home() {
             newBoard[y + 3][x] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 3][x] === 1) {
+          } else if (board[y + 3][x] === 1 || board[y + 3][x] === 0) {
             h += 1;
             setboard(newBoard);
           }
@@ -450,6 +451,7 @@ export default function Home() {
       }
     }
     if (board[x + 1] !== undefined && board[y][x + 1] === 2 / turnColor) {
+      console.log(h);
       newBoard[y][x] = turnColor;
       setTurnColor(2 / turnColor);
       setboard(newBoard);
@@ -722,145 +724,437 @@ export default function Home() {
         h = 0;
       }
     }
-    if (board[y + 1][x + 1] !== undefined && board[y + 1][x + 1] === 2 / turnColor) {
+    if (board[y - 1][x + 1] !== undefined && board[y - 1][x + 1] === 2 / turnColor) {
       newBoard[y][x] = turnColor;
       setTurnColor(2 / turnColor);
       setboard(newBoard);
       if (turnColor === 1) {
-        if (board[y + 1][x + 1] === 2) {
-          newBoard[y + 1][x + 1] = turnColor;
+        if (board[y - 1][x + 1] === 2) {
+          newBoard[y - 1][x + 1] = turnColor;
           setTurnColor(2 / turnColor);
           setboard(newBoard);
-        } else if (board[y + 1][x + 1] === 1) {
+        } else if (board[y - 1][x + 1] === 1) {
           h += 1;
           setboard(newBoard);
         }
         if (h !== 1) {
-          if (board[y + 2][x + 2] === 2) {
-            newBoard[y + 2][x + 2] = turnColor;
+          if (board[y - 2][x + 2] === 2) {
+            newBoard[y - 2][x + 2] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 2][x + 2] === 1) {
+          } else if (board[y - 2][x + 2] === 1) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 3][x + 3] === 2) {
-            newBoard[y + 3][x + 3] = turnColor;
+          if (board[y - 3][x + 3] === 2) {
+            newBoard[y - 3][x + 3] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 3][x + 3] === 1) {
+          } else if (board[y - 3][x + 3] === 1) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 4][x + 4] === 2) {
-            newBoard[y + 4][x + 4] = turnColor;
+          if (board[y - 4][x + 4] === 2) {
+            newBoard[y - 4][x + 4] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 4][x + 4] === 1) {
+          } else if (board[y - 4][x + 4] === 1) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 5][x + 5] === 2) {
-            newBoard[y + 5][x + 5] = turnColor;
+          if (board[y - 5][x + 5] === 2) {
+            newBoard[y - 5][x + 5] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 5][x + 5] === 1) {
+          } else if (board[y - 5][x + 5] === 1) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 6][x + 6] === 2) {
-            newBoard[y + 6][x + 6] = turnColor;
+          if (board[y - 6][x + 6] === 2) {
+            newBoard[y - 6][x + 6] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 6][x + 6] === 1) {
+          } else if (board[y - 6][x + 6] === 1) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 7][x + 7] === 2) {
-            newBoard[y + 7][x + 7] = turnColor;
+          if (board[y - 7][x + 7] === 2) {
+            newBoard[y - 7][x + 7] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 7][x + 7] === 1) {
+          } else if (board[y - 7][x + 7] === 1) {
             h += 1;
             setboard(newBoard);
           }
         }
         h = 0;
       } else if (turnColor === 2) {
-        if (board[y + 1][x + 1] === 1) {
-          newBoard[y + 1][x + 1] = turnColor;
+        if (board[y - 1][x + 1] === 1) {
+          newBoard[y - 1][x + 1] = turnColor;
           setTurnColor(2 / turnColor);
           setboard(newBoard);
-        } else if (board[y + 1][x + 1] === 2) {
+        } else if (board[y - 1][x + 1] === 2) {
           h += 1;
           setboard(newBoard);
         }
         if (h !== 1) {
-          if (board[y + 2][x + 2] === 1) {
-            newBoard[y + 2][x + 2] = turnColor;
+          if (board[y - 2][x + 2] === 1) {
+            newBoard[y - 2][x + 2] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 2][x + 2] === 2) {
+          } else if (board[y - 2][x + 2] === 2) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 3][x + 3] === 1) {
-            newBoard[y + 3][x + 3] = turnColor;
+          if (board[y - 3][x + 3] === 1) {
+            newBoard[y - 3][x + 3] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 3][x + 3] === 2) {
+          } else if (board[y - 3][x + 3] === 2) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 4][x + 4] === 1) {
-            newBoard[y + 4][x + 4] = turnColor;
+          if (board[y - 4][x + 4] === 1) {
+            newBoard[y - 4][x + 4] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 4][x + 4] === 2) {
+          } else if (board[y - 4][x + 4] === 2) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 5][x + 5] === 1) {
-            newBoard[y + 5][x + 5] = turnColor;
+          if (board[y - 5][x + 5] === 1) {
+            newBoard[y - 5][x + 5] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 5][x + 5] === 2) {
+          } else if (board[y - 5][x + 5] === 2) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 6][x + 6] === 1) {
-            newBoard[y + 6][x + 6] = turnColor;
+          if (board[y - 6][x + 6] === 1) {
+            newBoard[y - 6][x + 6] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 6][x + 6] === 2) {
+          } else if (board[y - 6][x + 6] === 2) {
             h += 1;
             setboard(newBoard);
           }
         }
         if (h !== 1) {
-          if (board[y + 7][x + 7] === 1) {
-            newBoard[y + 7][x + 7] = turnColor;
+          if (board[y - 7][x + 7] === 1) {
+            newBoard[y - 7][x + 7] = turnColor;
             setTurnColor(2 / turnColor);
             setboard(newBoard);
-          } else if (board[y + 7][x + 7] === 2) {
+          } else if (board[y - 7][x + 7] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        h = 0;
+      }
+    }
+    if (board[y - 1][x + 1] !== undefined && board[y - 1][x + 1] === 2 / turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(2 / turnColor);
+      setboard(newBoard);
+      if (turnColor === 1) {
+        if (board[y - 1][x + 1] === 2) {
+          newBoard[y - 1][x + 1] = turnColor;
+          setTurnColor(2 / turnColor);
+          setboard(newBoard);
+        } else if (board[y - 1][x + 1] === 1) {
+          h += 1;
+          setboard(newBoard);
+        }
+        if (h !== 1) {
+          if (board[y - 2][x + 2] === 2) {
+            newBoard[y - 2][x + 2] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 2][x + 2] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 3][x + 3] === 2) {
+            newBoard[y - 3][x + 3] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 3][x + 3] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 4][x + 4] === 2) {
+            newBoard[y - 4][x + 4] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 4][x + 4] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 5][x + 5] === 2) {
+            newBoard[y - 5][x + 5] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 5][x + 5] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 6][x + 6] === 2) {
+            newBoard[y - 6][x + 6] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 6][x + 6] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 7][x + 7] === 2) {
+            newBoard[y - 7][x + 7] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 7][x + 7] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        h = 0;
+      } else if (turnColor === 2) {
+        if (board[y - 1][x + 1] === 1) {
+          newBoard[y - 1][x + 1] = turnColor;
+          setTurnColor(2 / turnColor);
+          setboard(newBoard);
+        } else if (board[y - 1][x + 1] === 2) {
+          h += 1;
+          setboard(newBoard);
+        }
+        if (h !== 1) {
+          if (board[y - 2][x + 2] === 1) {
+            newBoard[y - 2][x + 2] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 2][x + 2] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 3][x + 3] === 1) {
+            newBoard[y - 3][x + 3] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 3][x + 3] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 4][x + 4] === 1) {
+            newBoard[y - 4][x + 4] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 4][x + 4] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 5][x + 5] === 1) {
+            newBoard[y - 5][x + 5] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 5][x + 5] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 6][x + 6] === 1) {
+            newBoard[y - 6][x + 6] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 6][x + 6] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y - 7][x + 7] === 1) {
+            newBoard[y - 7][x + 7] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y - 7][x + 7] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        h = 0;
+      }
+    }
+    if (board[y + 1][x - 1] !== undefined && board[y + 1][x - 1] === 2 / turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(2 / turnColor);
+      setboard(newBoard);
+      if (turnColor === 1) {
+        if (board[y + 1][x - 1] === 2) {
+          newBoard[y + 1][x - 1] = turnColor;
+          setTurnColor(2 / turnColor);
+          setboard(newBoard);
+        } else if (board[y + 1][x - 1] === 1) {
+          h += 1;
+          setboard(newBoard);
+        }
+        if (h !== 1) {
+          if (board[y + 2][x - 2] === 2) {
+            newBoard[y + 2][x - 2] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 2][x - 2] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 3][x - 3] === 2) {
+            newBoard[y + 3][x - 3] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 3][x - 3] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 4][x - 4] === 2) {
+            newBoard[y + 4][x - 4] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 4][x - 4] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 5][x - 5] === 2) {
+            newBoard[y + 5][x - 5] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 5][x - 5] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 6][x - 6] === 2) {
+            newBoard[y + 6][x - 6] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 6][x - 6] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 7][x - 7] === 2) {
+            newBoard[y + 7][x - 7] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 7][x - 7] === 1) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        h = 0;
+      } else if (turnColor === 2) {
+        if (board[y + 1][x - 1] === 1) {
+          newBoard[y + 1][x - 1] = turnColor;
+          setTurnColor(2 / turnColor);
+          setboard(newBoard);
+        } else if (board[y + 1][x - 1] === 2) {
+          h += 1;
+          setboard(newBoard);
+        }
+        if (h !== 1) {
+          if (board[y + 2][x - 2] === 1) {
+            newBoard[y + 2][x - 2] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 2][x - 2] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 3][x - 3] === 1) {
+            newBoard[y + 3][x - 3] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 3][x - 3] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 4][x - 4] === 1) {
+            newBoard[y + 4][x - 4] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 4][x - 4] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 5][x - 5] === 1) {
+            newBoard[y + 5][x - 5] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 5][x - 5] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 6][x - 6] === 1) {
+            newBoard[y + 6][x - 6] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 6][x - 6] === 2) {
+            h += 1;
+            setboard(newBoard);
+          }
+        }
+        if (h !== 1) {
+          if (board[y + 7][x - 7] === 1) {
+            newBoard[y + 7][x - 7] = turnColor;
+            setTurnColor(2 / turnColor);
+            setboard(newBoard);
+          } else if (board[y + 7][x - 7] === 2) {
             h += 1;
             setboard(newBoard);
           }
