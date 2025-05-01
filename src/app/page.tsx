6c4720abac04f31183b8ea3e401c344757d0793a
harumaki1220 <haruma181220@gmail.com>
2025-05-02 02:24:27 +0900

    ８方向を表す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,32 @@ export default function Home() {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setboard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const directions = [
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+  ];
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    for (let i: number = 0; i < 10; i++) {
-      console.log(i);
-      if (board[y + 1] !== undefined && board[y][x] === 2 / turnColor) {
-        newBoard[y][x] = turnColor;
-        setTurnColor(2 / turnColor);
-        setboard(newBoard);
-      }
-      y += 1;
-      if (board[y][x] === turnColor) break;
-      if (board[y][x] === 0) break;
+
+    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(2 / turnColor);
+      setboard(newBoard);
     }
   };
   return (
